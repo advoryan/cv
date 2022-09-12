d9241ba1e8886a8ccbf10ec66c33475b230a6d56
a.dvoryan <a.dvoryan@gmail.com>
2022-09-13 00:15:32 +0300

    feat: finish description

---
+++ script/carousel.js
@@ -0,0 +1,13 @@
+projectJSON = {
+    "Pet Shelter": "shelter",
+    "portfolio": "portfolio",
+    "tic-tac-toe": "tic-tac-toe",
+    "virtual keyboard": "virtual keyboard",
+    "css meme slider": "css meme slider",
+}
+
+const carousel = document.querySelector(".carousel");
+
+console.log(carousel);
+
+
